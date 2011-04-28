72124becd4aac29c644b624066078d460335dafd
Quinn Slack <sqs@cs.stanford.edu>
2011-04-27 20:59:07 -0700

    edit SRP info on profile/user-edit pages

--- plugin/tls-srp-authentication.php
+++ plugin/tls-srp-authentication.php
@@ -29,6 +29,12 @@ define('TLS_SRP_SHOW_DEBUG_INFO', true);
 
 define('TLS_SRP_PLUGIN_VERSION', '1.0');
 
+global $srp_metakeys, $srp_groups, $default_srp_group;;
+$srp_metakeys = array('srp_v', 'srp_s', 'srp_group');
+$srp_groups = array(1024, 1536, 2048);
+$default_srp_group = 1024;
+
+
 function tls_srp_authenticate($user) {
 
     $username = $_SERVER[TLS_SRP_AUTH_ENV_VAR];
@@ -62,9 +68,75 @@ if (!function_exists('auth_redirect')) {
     }
 }
 
+function tls_srp_get_user_srpinfo($user_id) {
+    global $srp_metakeys;
+    $srpinfo = array();
+    foreach ($srp_metakeys as $metakey) {
+        $srpinfo[$metakey] = get_user_meta($user_id, $metakey, true);
+    }
+    return $srpinfo;
+}
+
+function tls_srp_edit_user_profile($user) {
+    $srpinfo = tls_srp_get_user_srpinfo($user->ID);
+?>
+<h3>SRP user credentials</h3>
+<table class="form-table">
+<tr>
+    <th><label for="srp_v">SRP verifier</label></th>
+    <td><textarea name="srp_v" id="srp_v"><?php echo esc_attr($srpinfo['srp_v']) ?></textarea><span class="description">Base64-encoded</span></td>
+</tr>
+<tr>
+    <th><label for="srp_s">SRP salt</label></th>
+    <td><input type="text" name="srp_s" id="srp_s" value="<?php echo esc_attr($srpinfo['srp_s']) ?>" class="regular-text" /> <span class="description">Base64-encoded</span></td>
+</tr>
+<tr>
+    <th><label for="srp_group">SRP group size</label></th>
+    <td><select name="srp_group" id="srp_group">
+<?php
+    global $srp_groups, $default_srp_group;
+    foreach ($srp_groups as $group) {
+        echo "        <option value=\"$group\"";
+        if ($srpinfo['srp_group'] == $group ||
+            (!$srpinfo['srp_group'] && $group == $default_srp_group))
+            echo " selected=\"selected\"";
+        echo ">$group</option>\n";
+    }
+?>
+    </select></td>
+</tr>
+</table>
+<?php
+}
+
+function tls_srp_update_user_srpinfo($user_id) {
+    global $srp_metakeys, $srp_groups;
+    $metavals = array();
+    foreach ($srp_metakeys as $key) {
+        $metavals[$key] = (isset($_POST[$key]) ? $_POST[$key] : '');
+    }
+
+    // If srp_s and srp_v are empty, then don't set srp_group.
+    if (!$metavals['srp_v'] && !$metavals['srp_s'])
+        $metavals['srp_group'] = '';
+
+    foreach ($srp_metakeys as $key) {
+        $val = $metavals[$key];
+        if ($val) {
+            update_user_meta($user_id, $key, $val);
+        } else {
+            delete_user_meta($user_id, $key);
+        }
+    }
+}
 
 add_action('wp_logout', 'tls_srp_logout');
 add_action('init', 'tls_srp_authenticate');
+
+add_action('show_user_profile', 'tls_srp_edit_user_profile');
+add_action('edit_user_profile', 'tls_srp_edit_user_profile');
+add_action('edit_user_profile_update', 'tls_srp_update_user_srpinfo');
+add_action('personal_options_update', 'tls_srp_update_user_srpinfo');
 
 /* Add debug info to footer */
 if (TLS_SRP_SHOW_DEBUG_INFO)
